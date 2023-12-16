5571796da885264b86f200c559b85c37ddc541e8
Paranyu Akkhraviphanon <paranyuak@gmail.com>
2023-12-17 00:38:59 +0700

    layout change

--- LuckyDraw/script-luckydraw.js
+++ LuckyDraw/script-luckydraw.js
@@ -1,8 +1,8 @@
 
 function on() {
     document.getElementById("overlay").style.display = "block";
-  }
-  
+}
+
 function off() {
     document.getElementById("overlay").style.display = "none";
 }
@@ -12,72 +12,76 @@ function generateQRCode() {
     let qrcodeContainer = document.getElementById("qrcode");
     qrcodeContainer.innerHTML = "";
     new QRCode(qrcodeContainer, 'https://palm45.github.io/Luckydraw/Scan_OTP/otp.html');
-    
+
     document.getElementById("qrcode-container").style.display = "block";
 }
 
-function ButtonNextPage(){
+function ButtonNextPage() {
     document.getElementById("index").addEventListener("click", nexthome);
     function nexthome() {
         window.location.href = "index.html";
     }
 
     document.getElementById("listuser").addEventListener("click", nextlistuser);
-    function nextlistuser(){
+    function nextlistuser() {
         window.location.href = "listuser.html";
     }
 
     document.getElementById("listprize").addEventListener("click", nextlistprize);
-    function nextlistprize(){
+    function nextlistprize() {
         window.location.href = "listprize.html";
     }
 }
 
-var num1 = 5;
-var num2 = 5;
-const NowPrize =["จักรยาน","เบงๆ","เซี่ยงไฮ้","ไอโฟน","ลูกปิงปอง"];
-const NowCode =["Osd5l9","Px94wr","P1dfo5","2g0yP","0j8Eps"];
-const prize = ["จักรยาน","เบงๆ","เซี่ยงไฮ้","ไอโฟน","ลูกปิงปอง"];
-const code = ["Osd5l9","Px94wr","P1dfo5","2g0yP","0j8Eps"];
-function randomprize(){
+var num1 = 0;
+var num2 = 0;
+const NowRandom = [];
+const NowPrize = [];
+const NowCode = [];
+const prize = ["จักรยาน", "เบงๆ", "เซี่ยงไฮ้", "ไอโฟน", "ลูกปิงปอง"];
+const code = ["Osd5l9", "Px94wr", "P1dfo5", "2g0yP", "0j8Eps"];
+function randomprize() {
     let rand = Math.random() * prize.length;
     Prize = prize[Math.floor(rand)];
-    NowPrize[num1] = Prize;
-    num1++;
     return Prize
 }
 
-function randomcodeuser(){
+function randomcodeuser() {
     let rand = Math.random() * code.length;
     Code = code[Math.floor(rand)];
-    NowCode[num2] = Code;
-    num2++;
     return Code
 }
 
-function getrandom(){
-    document.getElementsByClassName('random-button')[0].innerHTML = 'รอ...'; 
+function getrandom() {
+    let RandomPrizeNow = '';
+    let RandomCodeNow = '';
+    document.getElementsByClassName('random-button')[0].innerHTML = 'รอ...';
     document.getElementsByClassName('random-button')[0].disabled = true;
-    intervalPrize = setInterval(function(){
-        document.getElementById('randomprize').innerHTML = randomprize();
+    intervalPrize = setInterval(function () {
+        RandomPrizeNow = randomprize();
+        document.getElementById('randomprize').innerHTML = RandomPrizeNow;
     });
-    intervalCode = setInterval(function(){
-        document.getElementById('randomcodeuser').innerHTML = randomcodeuser();
+    intervalCode = setInterval(function () {
+        RandomCodeNow = randomcodeuser();
+        document.getElementById('randomcodeuser').innerHTML = RandomCodeNow;
     });
 
-    setTimeout(function(){
+    setTimeout(function () {
         clearInterval(intervalPrize);
         clearInterval(intervalCode);
         document.getElementsByClassName('random-button')[0].innerHTML = 'สุ่มรางวัล';
         document.getElementsByClassName('random-button')[0].disabled = false;
-    },3000);
-}
-
-
-function NowRandomPrize(){
-    for (let i = 0; i < num1; i++) {
-        document.write(NowPrize[i] + "<br>");
-        document.write(NowCode[i] +"<br>");
-    }
-    
+        let RandomNow = RandomPrizeNow + ':' + RandomCodeNow;
+        NowPrize.push(RandomPrizeNow);
+        NowCode.push(RandomCodeNow);
+        for (let i = NowPrize.length-2; i >= 0; i--) {
+            document.getElementById('showrandom').innerHTML -= NowPrize[i];
+            document.getElementById('showrandom').innerHTML -= NowCode[i];
+        }
+        document.getElementById('showrandom').innerHTML = "";
+        for (let i = NowPrize.length-1; i >= 0; i--) {
+            document.getElementById('showrandom').innerHTML += NowPrize[i] + "<br>";
+            document.getElementById('showrandom').innerHTML += NowCode[i] + "</br>";
+        }
+    }, 3000);
 }
