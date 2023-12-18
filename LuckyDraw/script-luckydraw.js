a8b038ee401151c28209c33bd3dfd512233c4d1b
Paranyu Akkhraviphanon <paranyuak@gmail.com>
2023-12-18 21:42:54 +0700

    layout change

--- LuckyDraw/script-luckydraw.js
+++ LuckyDraw/script-luckydraw.js
@@ -52,6 +52,72 @@ function randomcodeuser() {
     return Code
 }
 
+const Confirm = {
+    open (options) {
+        options = Object.assign({}, {
+            title: '',
+            message: '',
+            okText: 'นำออก',
+            cancelText: 'ยกเลิก',
+            onok: function () {},
+            oncancel: function () {}
+        }, options);
+        const html = `
+            <div class="confirm">
+                <div class="confirm__window">
+                    <div class="confirm__titlebar">
+                        <span class="confirm__title">${options.title}</span>
+                        <button class="confirm__close">&times;</button>
+                    </div>
+                    <div class="confirm__content">${options.message}</div>
+                    <div class="confirm__buttons">
+                        <button class="confirm__button confirm__button--ok confirm__button--fill">${options.okText}</button>
+                        <button class="confirm__button confirm__button--cancel">${options.cancelText}</button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        const template = document.createElement('template');
+        template.innerHTML = html;
+
+        // Elements
+        const confirmEl = template.content.querySelector('.confirm');
+        const btnClose = template.content.querySelector('.confirm__close');
+        const btnOk = template.content.querySelector('.confirm__button--ok');
+        const btnCancel = template.content.querySelector('.confirm__button--cancel');
+
+        confirmEl.addEventListener('click', e => {
+            if (e.target === confirmEl) {
+                options.oncancel();
+                this._close(confirmEl);
+            }
+        });
+
+        btnOk.addEventListener('click', () => {
+            options.onok();
+            this._close(confirmEl);
+        });
+
+        [btnCancel, btnClose].forEach(el => {
+            el.addEventListener('click', () => {
+                options.oncancel();
+                this._close(confirmEl);
+            });
+        });
+
+        document.body.appendChild(template.content);
+    },
+
+    _close (confirmEl) {
+        confirmEl.classList.add('confirm--close');
+
+        confirmEl.addEventListener('animationend', () => {
+            document.body.removeChild(confirmEl);
+        });
+    }
+};
+
 function getrandom() {
     let RandomPrizeNow = '';
     let RandomCodeNow = '';
@@ -71,17 +137,37 @@ function getrandom() {
         clearInterval(intervalCode);
         document.getElementsByClassName('random-button')[0].innerHTML = 'สุ่มรางวัล';
         document.getElementsByClassName('random-button')[0].disabled = false;
-        let RandomNow = RandomPrizeNow + ':' + RandomCodeNow;
-        NowPrize.push(RandomPrizeNow);
-        NowCode.push(RandomCodeNow);
-        for (let i = NowPrize.length-2; i >= 0; i--) {
-            document.getElementById('showrandom').innerHTML -= NowPrize[i];
-            document.getElementById('showrandom').innerHTML -= NowCode[i];
-        }
-        document.getElementById('showrandom').innerHTML = "";
-        for (let i = NowPrize.length-1; i >= 0; i--) {
-            document.getElementById('showrandom').innerHTML += NowPrize[i] + "<br>";
-            document.getElementById('showrandom').innerHTML += NowCode[i] + "</br>";
-        }
-    }, 3000);
+        Confirm.open({
+            title: 'ยินดีด้วย!!',
+            message: String(RandomCodeNow),
+            onok: () => {
+                NowPrize.push(RandomPrizeNow);
+                NowCode.push(RandomCodeNow);
+                for(let i=0;i<prize.length;i++){
+                    if(prize[i] == RandomPrizeNow){
+                        delete prize[i];
+                        prize.splice(i, 1);
+                    }
+                }
+                for(let i=0;i<code.length;i++){
+                    if(code[i] == RandomCodeNow){
+                        delete code[i];
+                        code.splice(i,1);
+                    }
+                }
+                console.log(prize);
+                console.log(code);
+                
+                for (let i = NowPrize.length - 2; i >= 0; i--) {
+                    document.getElementById('showrandom').innerHTML -= NowPrize[i];
+                    document.getElementById('showrandom').innerHTML -= NowCode[i];
+                }
+                document.getElementById('showrandom').innerHTML = "";
+                for (let i = NowPrize.length - 1; i >= 0; i--) {
+                    document.getElementById('showrandom').innerHTML += NowPrize[i] + "<br>";
+                    document.getElementById('showrandom').innerHTML += NowCode[i] + "</br>";
+                }
+            },
+        })
+    }, 3000)
 }
