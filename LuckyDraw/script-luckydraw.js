8fb85c1bbdd3d4e09fd022cc1779a04abca29e40
Paranyu Akkhraviphanon <paranyuak@gmail.com>
2024-01-08 02:55:15 +0700

    layout change

--- LuckyDraw/script-luckydraw.js
+++ LuckyDraw/script-luckydraw.js
@@ -33,32 +33,75 @@ function ButtonNextPage() {
     }
 }
 
-var num1 = 0;
-var num2 = 0;
 const NowPrize = ["จักรยาน", "เบงๆ", "เซี่ยงไฮ้"];
 const NowCode = ["Osd5l9", "P1dfo5", "2g0yP"];
+const NowNotHere = [];
+const draw = [0, 0, 0];
 const countprize = [1, 2, 2];
 const prize = ["จักรยาน", "เบงๆ", "เซี่ยงไฮ้"]; //, "ไอโฟน", "ลูกปิงปอง"
 const code = ["Osd5l9", "Px94wr", "P1dfo5", "2g0yP", "0j8Eps"];
 const gmail = ["ggez@gmail.com", "sapook@gmail.com", "tyot@gmail.com", "Omega@gmail.com", "Tpose@gmail.com"];
+const UserGet = [false, false, false, false, false];
 
 function randomprize() {
-    let rand = Math.random() * prize.length;
-    if (prize.length == 0) {
+    check = 0;
+
+    for (let i = 0; i < draw.length; i++) {
+        if (draw[i] == countprize[i]) {
+            check++;
+        }
+    }
+
+    if (check == countprize.length) {
         Prize = "รางวัลหมดแล้ว!!";
     } else {
-        Prize = prize[Math.floor(rand)];
+        while (true) {
+            let rand = Math.random() * prize.length;
+            random = Math.floor(rand);
+            if (countprize[random] > draw[random]) {
+                Prize = prize[random];
+                break;
+            }
+        }
     }
 
     return Prize
 }
 
 function randomcodeuser() {
-    let rand = Math.random() * code.length;
-    if (code.length == 0) {
+    check = 0;
+
+    for (let i = 0; i < code.length; i++) {
+        if (UserGet[i] == true) {
+            check++;
+        }
+    }
+    for (let i = 0; i < NowNotHere.length; i++) {
+        check++;
+    }
+
+    if (check == code.length) {
         Code = "ไม่มีคนแล้ว!!";
     } else {
-        Code = code[Math.floor(rand)];
+        checkrandom = false;
+        while (true) {
+            let rand = Math.random() * code.length;
+            random = Math.floor(rand);
+            if (UserGet[random] == false) {
+                if(NowNotHere.length>0){
+                    for(let i = 0; i < NowNotHere.length; i++){
+                        if(NowNotHere[i] == code[random]){
+                            checkrandom = true
+                        }
+                    }
+                }
+                if(checkrandom == false){
+                    Code = code[random];
+                    break;
+                }
+                checkrandom = false;
+            }
+        }
     }
 
     return Code
@@ -70,8 +113,10 @@ const Confirm = {
             title: '',
             message: '',
             okText: 'นำออก',
+            removeText: 'ไม่อยู่',
             cancelText: 'ยกเลิก',
             onok: function () { },
+            onremove: function () { },
             oncancel: function () { }
         }, options);
         const html = `
@@ -84,6 +129,7 @@ const Confirm = {
                     <div class="confirm__content">${options.message}</div>
                     <div class="confirm__buttons">
                         <button class="confirm__button confirm__button--ok confirm__button--fill">${options.okText}</button>
+                        <button class="confirm__button confirm__button--remove confirm__button--fill">${options.removeText}</button>
                         <button class="confirm__button confirm__button--cancel">${options.cancelText}</button>
                     </div>
                 </div>
@@ -98,6 +144,7 @@ const Confirm = {
         const btnClose = template.content.querySelector('.confirm__close');
         const btnOk = template.content.querySelector('.confirm__button--ok');
         const btnCancel = template.content.querySelector('.confirm__button--cancel');
+        const btnRemove = template.content.querySelector('.confirm__button--remove');
 
         confirmEl.addEventListener('click', e => {
             if (e.target === confirmEl) {
@@ -109,6 +156,12 @@ const Confirm = {
         btnOk.addEventListener('click', () => {
             options.onok();
             btnOk.disabled = true;
+            this._close(confirmEl);
+        });
+
+        btnRemove.addEventListener('click', () => {
+            options.onremove();
+            btnRemove.disabled = true;
             this._close(confirmEl);
         });
 
@@ -155,28 +208,41 @@ function getrandom() {
             title: 'ยินดีด้วย!!',
             message: String(RandomCodeNow),
             onok: () => {
-                if (prize.length != 0 && code.length != 0) {
+                var checkprize = 0;
+                var checkUserGet = 0;
+                for (let i = 0; i < draw.length; i++) {
+                    if (draw[i] == countprize[i]) {
+                        checkprize++;
+                    }
+                }
+                for(let i = 0; i < UserGet.length; i++) {
+                    if (UserGet[i] == true) {
+                        checkUserGet++;
+                    }
+                }
+                for(let i = 0; i < NowNotHere.length; i++) {
+                    checkUserGet++;
+                }
+                if (checkprize != countprize.length && checkUserGet != code.length) {
                     NowPrize.push(RandomPrizeNow);
                     NowCode.push(RandomCodeNow);
                     for (let i = 0; i < prize.length; i++) {
                         if (prize[i] == RandomPrizeNow) {
-                            if (countprize[i] > 1) {
-                                countprize[i] -= 1;
-                            } else {
-                                countprize[i] -= 1;
-                                prize.splice(i, 1);
-                                countprize.splice(i, 1);
+                            if (countprize[i] > draw[i]) {
+                                draw[i]++;
                             }
                         }
                     }
                     for (let i = 0; i < code.length; i++) {
                         if (code[i] == RandomCodeNow) {
-                            code.splice(i, 1);
+                            UserGet[i] = true;
                         }
                     }
-                    console.log(countprize.length);
-                    console.log(prize.length);
-                    console.log(code.length);
+
+                    console.log(prize);
+                    console.log(draw);
+                    console.log(code);
+                    console.log(UserGet);
 
                     for (let i = NowPrize.length - 2; i >= 0; i--) {
                         document.getElementById('showrandom').innerHTML -= NowPrize[i];
@@ -190,6 +256,21 @@ function getrandom() {
                 }
 
             },
+            onremove: () => {
+                check = 0;
+                for (let i = 0; i < code.length; i++) {
+                    if (UserGet[i] == true) {
+                        check++;
+                    }
+                }
+                for (let i = 0; i < NowNotHere.length; i++) {
+                    check++;
+                }
+                if(check < code.length) {
+                   NowNotHere.push(RandomCodeNow);
+                   console.log(NowNotHere);
+                }
+            }
         })
     }, 3000)
 }
@@ -200,14 +281,81 @@ function ShowRandom() {
     }
 }
 
+const NotHere = {
+    open(options) {
+        options = Object.assign({}, {
+            title: 'รายการคนที่ไม่อยู่',
+            message: '',
+            oncancel: function () { }
+        }, options);
+        const html = `
+            <div class="confirm">
+                <div class="confirm__windowNotHere">
+                    <div class="confirm__titlebarNotHere">
+                        <span class="confirm__title">${options.title}</span>
+                        <button class="confirm__close">&times;</button>
+                    </div>
+                    <div class="confirm__contentNotHere">${options.message}</div>
+                </div>
+            </div>
+        `;
+
+        const template = document.createElement('template');
+        template.innerHTML = html;
+
+        // Elements
+        const confirmEl = template.content.querySelector('.confirm');
+        const btnClose = template.content.querySelector('.confirm__close');
+
+        confirmEl.addEventListener('click', e => {
+            if (e.target === confirmEl) {
+                options.oncancel();
+                this._close(confirmEl);
+            }
+        });
+
+        [btnClose].forEach(el => {
+            el.addEventListener('click', () => {
+                options.oncancel();
+                el.disabled = true;
+                this._close(confirmEl);
+            });
+        });
+
+        document.body.appendChild(template.content);
+    },
+
+    _close(confirmEl) {
+        confirmEl.classList.add('confirm--close');
+
+        confirmEl.addEventListener('animationend', () => {
+            document.body.removeChild(confirmEl);
+        });
+    }
+};
+function ShowNotHereTable(){
+    table = `<table id="myTableNotHere" class="display" style="width: 100%;"><thead><tr>`;
+    for(var i = 0; i <NowNotHere.length;i++){
+        table += `<td>` + NowNotHere[i] + `</td>`
+        table += `<td><button class="remove-nothere">นำออก</button></td>`
+        table += `</tr><tr>`
+    }
+    table += `</tr>`
+    table += `</thead></table>`
+    return table;
+}
+function ShowNotHere(){
+    NotHere.open({
+        message: ShowNotHereTable(),
+    })
+}
+
 
 var type = 0;
 function ShowAllUser() {
     var mytable = "<tr>"
     for (let i = 0; i < gmail.length; i++) {
-        mytable += "<td>"+(i+1)+"</td></td>"
-        mytable += "<td>" + gmail[i] + "</td>"
-        mytable += "<td>" + code[i] + "</td>"
+        mytable += "<td>" + (i + 1) + "</td></td>"
         for (let j = 0; j < NowCode.length; j++) {
             if (code[i] == NowCode[j]) {
                 check = true;
@@ -219,6 +367,7 @@ function ShowAllUser() {
             mytable += "<td>" + "-" + "</td>"
         }
         check = false;
+        mytable += "<td>" + code[i] + "</td>"
         mytable += "</tr><tr>"
     }
     mytable += "</tr>";
@@ -229,7 +378,7 @@ function SelectType(select) {
 }
 function SearchTable() {
     var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("search");
+    input = document.getElementById("SearchInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
@@ -247,49 +396,109 @@ function SearchTable() {
         }
     }
 }
+function ShowSearch() {
+    var x = document.getElementById("SearchInput");
+    var x2 = document.getElementById("SearchType");
+
+    document.getElementById("PrizeSelect").style.display = "none";
+
+    x.style.display = "block";
+    x2.style.display = "block";
+}
+function ShowPrizeSelect() {
+    var x = document.getElementById("PrizeSelect");
+
+    document.getElementById("SearchInput").style.display = "none";
+    document.getElementById("SearchType").style.display = "none";
+
+    x.style.display = "block";
+    console.log(document.getElementById("SearchType").value);
+}
+function PrizeSelect() {
+    var PrizeSelect = "<option name='choice0' value='-1'>แสดงทั้งหมด</option>";
+    PrizeSelect += "<option name='choice0' value='-2'>ยังไม่ได้</option>"
+    for (let i = 0; i < prize.length; i++) {
+        PrizeSelect += "<option name='choice" + (i + 1) + "' value=" + i + ">" + prize[i] + "</option>";
+    }
+    document.write(PrizeSelect);
+}
+function SearchPrize(num) {
+    var table, tr, td, i, txtValue;
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    if (num == "-1") {
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[2];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                tr[i].style.display = "";
+            }
+        }
+    } else if (num == "-2") {
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[2];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf("-") > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    } else {
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[2];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(prize[num]) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+}
 
 
+var count = 0;
 function AddPrize() {
     let name = document.getElementById("add-prize").value;
     let num = document.getElementById("add-numprize").value;
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(table.rows.length);
-    var i = table.rows.length - 3;
+    if (name.length > 0) {
+        var table = document.getElementById("myTable");
+        var row = table.insertRow(table.rows.length);
+        var i = table.rows.length - 3 + count;
 
-    row.insertCell(0).innerHTML = table.rows.length - 2;
-    row.insertCell(1).innerHTML = '<td><input type="text" id="EditPrize_' + i + '" class="prize_data" value="'+ name+ '" readonly></input></td>';
-    row.insertCell(2).innerHTML = '<td><input type="number" type="number" min="1" max="9999" step="1" id="EditNumber_' + i + '" class="numprize_data" value="'+ num+ '" readonly></input></td>';
-    row.insertCell(3).innerHTML = '<span id="edit_' + i + '"> <button class="edit-button" onclick="Editbutton(' + i + ')">แก้ไข</button></span>'
-        + '<span id="save_' + i + '"> <button class="save-button" onclick="EditSave(' + i + ')">บันทึก</button></span>'
-        + '<span id="cancel_' + i + '"> <button class="cancel-button" onclick="Editcancel(' + i + ')">ยกเลิก</button></span>'
-        + '<button class="delete-button" onclick="DeletePrize(this,' + i + ')">ลบ</button>';
+        row.insertCell(0).innerHTML = table.rows.length - 2 + count;
+        row.insertCell(1).innerHTML = '<td><input type="text" class="prize_data" value="' + name + '" readonly></input></td>';
+        row.insertCell(2).innerHTML = '<td><input type="text" type="number" min="1" max="9999" step="1" class="numprize_data" value="' + num + '" readonly></input></td>';
+        row.insertCell(3).innerHTML = '<td><input type="text" class="numprize_data" value="' + 0 + '" readonly></input></td>';
+        row.insertCell(4).innerHTML = '<button class="delete-button" onclick="DeletePrize(this,' + i + ')">ลบ</button>';
 
-    prize.push(name);
-    countprize.push(parseInt(num));
+        prize.push(name);
+        countprize.push(parseInt(num));
+        draw.push(0);
 
-    document.getElementById("add-prize").value = "";
-    document.getElementById("add-numprize").value = 1;
+        document.getElementById("add-prize").value = "";
+        document.getElementById("add-numprize").value = 1;
 
-    for (let i = 0; i < prize.length; i++) {
-        const btn = document.getElementById('edit_' + i);
-        const btn2 = document.getElementById('save_' + i);
-        const btn3 = document.getElementById('cancel_' + i);
-
-        btn.hidden = false;
-        btn2.hidden = true;
-        btn3.hidden = true;
+        console.log(prize);
+        console.log(countprize);
+        console.log(draw);
     }
 }
 function ShowAllPrizes() {
     var mytable = "<tr>"
     for (let i = 0; i < prize.length; i++) {
-        mytable += "<td>"+(i+1)+"</td>"
-        mytable += "<td><input type='text' id='EditPrize_" + i + "' class='prize_data' value=" + prize[i] + " readonly></input></td>"
-        mytable += "<td><input type='number' min='1' max='9999' step='1' id='EditNumber_" + i + "' class='numprize_data'  value=" + countprize[i] + " readonly></input></td>";
+        mytable += "<td>" + (i + 1) + "</td>"
+        mytable += "<td><input type='text' class='prize_data' value=" + prize[i] + " readonly></input></td>"
+        mytable += "<td><input type='text' min='1' max='9999' step='1' class='numprize_data'  value=" + countprize[i] + " readonly></input></td>";
+        mytable += "<td><input type='text' class='numprize_data' value=" + draw[i] + " readonly></input></td>"
         mytable += "<td>";
-        mytable += "<span id='edit_" + i + "'>" + "<button class='edit-button' onclick='Editbutton(" + i + ")'>แก้ไข</button>" + "</span>";
-        mytable += "<span id='save_" + i + "'>" + "<button class='save-button' onclick='EditSave(" + i + ")'>บันทึก</button>" + "</span>";
-        mytable += "<span id='cancel_" + i + "'>" + "<button class='cancel-button' onclick='Editcancel(" + i + ")'>ยกเลิก</button>" + "</span>";
         mytable += "<button class='delete-button' onclick='DeletePrize(this," + i + ")'>ลบ</button>";
         mytable += "</td>"
         mytable += "</tr><tr>"
@@ -297,83 +506,16 @@ function ShowAllPrizes() {
     mytable += "</tr>"
 
     document.write(mytable);
-
-    for (let i = 0; i < prize.length; i++) {
-        const btn = document.getElementById('edit_' + i);
-        const btn2 = document.getElementById('save_' + i);
-        const btn3 = document.getElementById('cancel_' + i);
-
-        btn.hidden = false;
-        btn2.hidden = true;
-        btn3.hidden = true;
-    }
-}
-function Editbutton(Rownum) {
-    console.log(prize)
-
-    const editPrizeInput = document.getElementById('EditPrize_' + Rownum);
-    const editNumberInput = document.getElementById('EditNumber_' + Rownum);
-
-    const btn = document.getElementById('edit_' + Rownum);
-    const btn2 = document.getElementById('save_' + Rownum);
-    const btn3 = document.getElementById('cancel_' + Rownum);
-
-    editPrizeInput.removeAttribute('readonly');
-    editNumberInput.removeAttribute('readonly');
-
-    btn.hidden = true;
-    btn2.hidden = false;
-    btn3.hidden = false;
-}
-
-
-function EditSave(Rownum) {
-    const btn = document.getElementById('edit_' + Rownum);
-    const btn2 = document.getElementById('save_' + Rownum);
-    const btn3 = document.getElementById('cancel_' + Rownum);
-
-    btn.hidden = false;
-    btn2.hidden = true;
-    btn3.hidden = true;
-
-    const editPrizeInput = document.getElementById('EditPrize_' + Rownum);
-    const editNumberInput = document.getElementById('EditNumber_' + Rownum);
-
-    console.log(editPrizeInput.value)
-
-    prize[Rownum] = editPrizeInput.value;
-    countprize[Rownum] = parseInt(editNumberInput.value);
-
-    console.log(prize,countprize)
-
-    editPrizeInput.setAttribute('readonly', 'readonly');
-    editNumberInput.setAttribute('readonly', 'readonly');
-}
-
-
-function Editcancel(Rownum) {
-    const btn = document.getElementById('edit_' + Rownum);
-    const btn2 = document.getElementById('save_' + Rownum);
-    const btn3 = document.getElementById('cancel_' + Rownum);
-
-    btn.hidden = false;
-    btn2.hidden = true;
-    btn3.hidden = true;
-
-    const editPrizeInput = document.getElementById('EditPrize_' + Rownum);
-    const editNumberInput = document.getElementById('EditNumber_' + Rownum);
-    
-    editPrizeInput.value = prize[Rownum];
-    editNumberInput.value = countprize[Rownum];
-   
-    editPrizeInput.setAttribute('readonly', 'readonly');
-    editNumberInput.setAttribute('readonly', 'readonly');
 }
 function DeletePrize(button, i) {
     let row = button.parentNode.parentNode;
     row.parentNode.removeChild(row);
     prize.splice(i, 1);
     countprize.splice(i, 1);
+    draw.splice(i, 1);
+    count++;
+
     console.log(prize);
     console.log(countprize);
+    console.log(draw);
 }
